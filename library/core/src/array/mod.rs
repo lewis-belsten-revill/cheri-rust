15d59b4744a79ce81a215d1b0f532b378c4a5757
Lewis Belsten-Revill <lewis.belsten-revill@codasip.com>
2026-04-24 10:28:01 +0100

    TEMP: Expand length of default impls on static arrays

--- library/core/src/array/mod.rs
+++ library/core/src/array/mod.rs
@@ -504,7 +504,7 @@ macro_rules! array_impl_default {
     };
 }
 
-array_impl_default! {32, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
+array_impl_default! {40, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
 
 impl<T, const N: usize> [T; N] {
     /// Returns an array of the same size as `self`, with function `f` applied to each element
