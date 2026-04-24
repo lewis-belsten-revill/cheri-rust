170a41cb6bae8a5a0dffe5e9b260d1bf84686e2d
Lewis Belsten-Revill <lewis.belsten-revill@codasip.com>
2026-04-24 10:29:30 +0100

    TEMP: Disable failing debug assertion (upstream bug)

--- compiler/rustc_middle/src/hir/map.rs
+++ compiler/rustc_middle/src/hir/map.rs
@@ -988,7 +988,6 @@ impl<'tcx> TyCtxt<'tcx> {
             }
             _ => self.hir_span_with_body(hir_id),
         };
-        debug_assert_eq!(span.ctxt(), self.hir_span_with_body(hir_id).ctxt());
         span
     }
 
